7e1b4e335965b936002925e6f043ed0e882a39a1
tmahon94 <86294939+tmahon94@users.noreply.github.com>
2025-03-23 00:05:20 +0000

    Part 2 finished

--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@
         <li>
             <a href="read.php"><strong>Read</strong></a> - find a user
         </li>
+        <li>
+            <a href="update.php"><strong>Update</strong></a> - edit a user
+        </li>
+        <li>
+            <a href="delete.php"><strong>Delete</strong></a> - delete a user
     </ul>
     <?php include "templates/footer.php"; ?>
 
